8afe05430e732f9fc6997cb4da78d7ebc0fb40f6
schraderSimon <simonsch@student.matnat.uio.no>
2021-12-08 16:20:08 +0100

    Mini change to runall (correct input params)

--- code/runall.py
+++ code/runall.py
@@ -7,7 +7,7 @@ def delcases(directory, num_MC, years):
         files=glob.glob("%s/*_%d_*_%d_*.csv"%(directory,year,num_MC))
         for file in files:
             remove(file)
-num_MC=100 #The number of MC steps n
+num_MC=10000 #The number of MC steps n
 rngseed=0 #Change to get different data. 0 as starting seed is used in all simulations
 years=[2020,2022] #Running different years is possible, but changes need to be made in the plot file or the file naming convention
 #Step 1: Run "grab_data" files. They create the mathematical VAR model based on the data and also plot some graphs that (not all...) are in the report.
